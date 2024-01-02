bbfa37ec65a2c5edd59337b0d1d6e446cc3d9cec
wemersonm <wemersonmoura02@gmail.com>
2024-01-02 00:14:51 -0300

    add_banner_offer_in_homepage

--- frontend/src/components/offer-banner/OfferBanner.vue
+++ frontend/src/components/offer-banner/OfferBanner.vue
@@ -17,4 +17,26 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@media (max-width: 778px) {
+  img {
+    height: 280px;
+  }
+
+  @media (max-width: 528px) {
+    img {
+      height:230px;
+    }
+  }
+  @media (max-width: 328px) {
+    img {
+      height:180px;
+    }
+  }
+  @media (max-width: 232) {
+    img {
+      height:auto;
+    }
+  }
+}
+</style>
