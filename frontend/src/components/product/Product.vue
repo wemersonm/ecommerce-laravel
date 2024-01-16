c7cec52be905b9020c3630c5b1385e158e670130
wemersonm <wemersonmoura02@gmail.com>
2024-01-15 23:57:35 -0300

    add-product-page

--- frontend/src/components/product/Product.vue
+++ frontend/src/components/product/Product.vue
@@ -25,18 +25,53 @@
       </div>
       <div class="col-5 border border-success">
         <div>
-          <span class="fs-5">{{ product.name }}</span>
+          <div class="d-flex flex-column">
+            <span class="fs-5">{{ product.name }}</span>
+            <span><a class="text-secondary" :href="'/marcas/' + product.brand.slug">{{ product.brand.name }}</a></span>
+          </div>
           <RatingProduct :rating="product.rating" :qtyReview="product.qtyReview" />
 
           <div class="d-flex flex-column mt-3">
-            <span class="text-secondary">R$<span class=" text-decoration-line-through">
+            <span class="text-secondary">R$ <span class=" text-decoration-line-through">
                 {{ product.price * 1.1 }}</span></span>
-            <span class="fs-4">R$ {{ product.price }}</span>
+            <span class="fs-3 text-danger fw-bold">R$ {{ toBRL((product.price - (product.price * 0.05)))
+            }}</span>
+            <span class="text-secondary">À vista no PIX com até 5% OFF</span>
           </div>
 
-          
+          <div class="mt-3 text-secondary small">
+            <b>R$ {{ toBRL(product.price) }}</b>
+            <p class="m-0">Em até 10x de <span class="fw-semibold">R$ {{ toBRL(product.price / 10) }}</span> juros no
+              cartão</p>
+            <p>Ou em 1x no cartão com até <b>5%</b> OFF</p>
+          </div>
+
+          <div class="d-flex gap-4">
+            <button class="btn btn-danger px-4">
+              <i class="bi bi-cart2 fs-5"></i>
+              <span class="fs-5 ms-2 ">Comprar</span>
+            </button>
+            <button class="btn btn-danger">
+              <i class="bi bi-cart-plus fs-5"></i>
+            </button>
+          </div>
+
+          <div class="d-flex mt-4">
+            <form method="post" @submit.prevent="submitFreight">
+              <span class="small fw-bold mb-2 d-block">Consulte o prazo de entrega</span>
+              <div class="input-group">
+                <input type="text" id="inputFreight" class="form-control" placeholder="Insira o CEP" v-mask="'#####-###'">
+                <button class="btn btn-outline-danger">Consultar</button>
+              </div>
+            </form>
+          </div>
+
         </div>
       </div>
+    </div>
+
+    <div class="mt-5">
+      <div v-html="product.description"></div>
     </div>
   </div>
 </template>
@@ -45,6 +80,7 @@
 // @ts-ignore
 import RatingProduct from './RatingProduct.vue';
 import Breadcrumb from '../utils/Breadcrumb.vue';
+import { mask } from 'vue-the-mask'
 
 export default {
   data() {
@@ -63,10 +99,16 @@ export default {
           name: 'Placa de Video',
           slug: 'placa-de-video'
         },
+        brand: {
+          name: 'Zotac',
+          slug: 'zotac',
+        },
         price: 989.90,
+
         rating: 4.5,
         qtyReview: 27,
-
+        stock: 250,
+        description: '<h2>Pega a rajada de 30 só eco vindo barulhento</h2>',
 
       },
 
@@ -74,10 +116,18 @@ export default {
 
     };
   },
+  methods: {
+    toBRL(number) {
+      return number.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    }
+  },
   mounted() {
     this.slug = this.$route?.params?.slug;
   },
-  components: { Breadcrumb, RatingProduct }
+  components: { Breadcrumb, RatingProduct },
+  directives: {
+    mask,
+  }
 }
 </script>
 
