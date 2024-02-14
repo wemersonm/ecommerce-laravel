01fbeb4fee087b7f31d28118e9e03444a2559dd1
wemersonm <wemersonmoura02@gmail.com>
2024-02-14 16:54:10 -0300

    add_adress migration and model

---
+++ backend/database/migrations/2024_02_14_153914_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users', 'id')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->string('name');
+            $table->tinyInteger('address_type');
+            $table->string('Recipient');
+            $table->string('cep');
+            $table->string('street');
+            $table->string('number');
+            $table->string('complement')->nullable();
+            $table->string('neighborhood');
+            $table->string('city');
+            $table->string('uf', 2);
+            $table->string('reference')->nullable();
+            $table->boolean('main');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
