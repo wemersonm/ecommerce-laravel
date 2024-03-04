1b233e4db36e638fdfb600c96e9e9291fedc554a
Wemerson Moura <73254098+wemersonm@users.noreply.github.com>
2024-03-03 23:40:46 -0300

    Delete run directory

--- run/nrd.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM Entrar na primeira pasta e executar um comando
-cd ../frontend
-npm run dev
--- run/redis.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-REM Entrar no WSL e executar três comandos
-wsl bash -c "sudo service redis-server start && sleep 1 && redis-cli"
-
