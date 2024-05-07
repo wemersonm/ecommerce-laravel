a4659d778c83704334c3e788c239bce198e32b43
wemersonm <wemersonmoura02@gmail.com>
2024-05-07 19:53:12 -0300

    refactor banners resource

--- backend/app/Http/Controllers/BannerController.php
+++ backend/app/Http/Controllers/BannerController.php
@@ -2,20 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Throwable;
 use App\Models\Banner;
+use App\Rules\SectionBanner;
 use Illuminate\Http\Request;
+use App\Traits\ResponseService;
+use Illuminate\Support\Facades\Cache;
 
 class BannerController extends Controller
 {
+    use ResponseService;
     public function index(Request $request)
     {
-        $request->validate([
-            'section' => ['regex:/^[a-zA-Z0-1\-]+$/'],
+        $request_data = $request->validate([
+            'section' => [new SectionBanner,],
         ]);
-
-        $banners = Banner::where('active', true)
-            ->where('section', $request->input('section'))
-            ->orderBy('order', 'asc')->get();
-        return $banners;
+        try {
+            $banners_cache = json_decode(Cache::get('banners_cache'), true);
+            if ($banners_cache) {
+                $banners_section = $banners_cache[$request_data['section']];
+            }
+            if (!isset($banners_section) || !$banners_section) {
+                $banners_section = Banner::where('active', true)
+                    ->where('section', $request_data['section'])
+                    ->orderBy('order', 'asc')->get();
+            }
+            return $this->responseSuccess($banners_section);
+        } catch (Throwable $th) {
+            return $this->responseError($th, 'error when get banners');
+        }
     }
 }
